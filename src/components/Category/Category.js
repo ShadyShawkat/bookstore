387acefbab57ca849dd39f79e9d7afd930e80a7b
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-17 22:54:48 +0200

    Add category component

---
+++ src/components/Category/Category.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Category.module.css';
+
+const Category = () => (
+  <div className={styles.Category}>
+    Category Component
+  </div>
+);
+
+Category.propTypes = {};
+
+Category.defaultProps = {};
+
+export default Category;
