387585712349cbcdcd1bd094c12b915098b4b507
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-17 22:55:39 +0200

    Setup basic website structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import styles from './App.module.css';
+
+import Nav from './components/Nav/Nav';
+import BookList from './components/BookList/BookList';
+import CategoryList from './components/CategoryList/CategoryList';
+
+const dummyBooksData = [
+  {
+    id: 1,
+    name: 'Nice book',
+    category: 'Action',
+  },
+  {
+    id: 2,
+    name: 'Great book',
+    category: 'Drama',
+  },
+  {
+    id: 3,
+    name: 'Best book',
+    category: 'Sci-Fi',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.wrapper}>
+        <Routes>
+          <Route path="/" element={<BookList books={dummyBooksData} />} />
+          <Route path="/categories" element={<CategoryList />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
