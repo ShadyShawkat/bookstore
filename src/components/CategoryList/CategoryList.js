eeb76f5f872997ecc7dfe58f7ed208fe649a0b07
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-17 22:55:04 +0200

    Add categoryList component

---
+++ src/components/CategoryList/CategoryList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './CategoryList.module.css';
+
+const CategoryList = () => (
+  <div className={styles['category-list']}>Under construction</div>
+);
+
+export default CategoryList;
