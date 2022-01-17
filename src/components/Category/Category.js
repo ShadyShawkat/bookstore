b622a4519ee8c13cde8cc666b9189fa6616e940b
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-17 23:48:48 +0200

    Fix linters

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styles from './Category.module.css';
 
 const Category = () => (
-  <div className={styles.Category}>
-    Category Component
-  </div>
+  <div className={styles.category}>Category Component</div>
 );
 
-Category.propTypes = {};
+// Category.propTypes = {};
 
-Category.defaultProps = {};
+// Category.defaultProps = {};
 
 export default Category;
