12cb2c3be91f182febe6c3c28ffd6330d79e733c
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-19 19:16:28 +0200

    Implement async action using thunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,10 @@
+/* eslint-disable operator-linebreak */
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookStore/books/FETCH_BOOKS';
+
+const baseURL =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/bHuFSNeqeLPXOroi5beE';
 
 const initialState = [];
 
@@ -12,6 +17,22 @@ export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+const fetchBooks = (payload) => ({
+  type: FETCH_BOOKS,
+  payload,
+});
+
+export const fetchBooksAsync = () => (dispatch) => {
+  fetch(`${baseURL}/books`)
+    .then((data) => data.json())
+    .then((data) => {
+      dispatch(fetchBooks(data));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
